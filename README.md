2a647c3ff5df8f4ab7c7c8492f38b1560c22df91
Elliott <elliottohj44@gmail.com>
2021-05-25 20:33:02 -0400

    fixed the time output for chat messages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # conjuring-backend
 
-Server for the real-time chat feature of my [conjuring](https://github.com/Elliohknow/conjuring) web app, which you can check out at https://conjuring.info/
+Server for the real-time chat feature of my [conjuring](https://github.com/elliottandjones/conjuring) web app, which you can check out at https://conjuring-2b5a2.firebaseapp.com/
 
 Uses Node.js, Express.js, Socket.io
--- messages.js
+++ messages.js
@@ -1,47 +1,56 @@
-const { rollAttack, rollDamage } = require("./roll");
+const { rollAttack, rollDamage } = require("./roll")
 
 const conjureChatMessage = (name, text) => {
-	return {
-		name,
-		text,
-		createdAt: getTheTime(),
-		isAction: false
-	};
-};
+  return {
+    name,
+    text,
+    createdAt: getTheTime(),
+    isAction: false,
+  }
+}
 
 const conjureRollMessage = (name, creatureName, action) => {
-	return {
-		name,
-		creatureName,
-		action,
-		rolls: getRolls(action),
-		createdAt: getTheTime(),
-		isAction: true
-	};
-};
+  return {
+    name,
+    creatureName,
+    action,
+    rolls: getRolls(action),
+    createdAt: getTheTime(),
+    isAction: true,
+  }
+}
 
 function getRolls(action) {
-	let isCrit = false;
-	const att = rollAttack();
-	if (att === 20 || att === "20") {
-		isCrit = true;
-	}
-	const dmg = rollDamage(action.damage_dice, action.damage_bonus, isCrit);
-	// console.log("{att, dmg, isCrit}: ",{att: att, dmg: dmg, isCrit: isCrit})
-	return { att, dmg, isCrit }; // { number, array, boolean }
+  let isCrit = false
+  const att = rollAttack()
+  if (att === 20 || att === "20") {
+    isCrit = true
+  }
+  const dmg = rollDamage(action.damage_dice, action.damage_bonus, isCrit)
+  // console.log("{att, dmg, isCrit}: ",{att: att, dmg: dmg, isCrit: isCrit})
+  return { att, dmg, isCrit } // { number, array, boolean }
 }
 
 function getTheTime() {
-	const date = new Date(Date.now());
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
-	return `${hours > 12 ? hours - 12 : hours < 1 ? 12 : hours}:${("0" + minutes).slice(-2)} ${hours >= 12 ? "PM" : "AM"}`;
+  const rightNow = new Date();
+  return rightNow.toLocaleTimeString([],{ 
+    hour12: true,
+    hour: 'numeric',
+    minute: '2-digit',
+  })
 }
-// NOT using -> date.toLocaleTimeString([], { hour12:true, hour:'2-digit', minute:'2-digit' });
+// doesn't return time based on locale
+// function getTheTime() {
+//   const date = new Date(Date.now())
+//   let hours = date.getHours()
+//   let minutes = date.getMinutes()
+//   return `${hours > 12 ? hours - 12 : hours < 1 ? 12 : hours}:${("0" + minutes).slice(-2)} ${hours >= 12 ? "PM" : "AM"}`
+// }
+// NOT using -> date.toLocaleTimeString([], { hour12:true, hour:'2-digit', minute:'2-digit' }); <== false
 // Why not? Because MDN says it's experimental, and the options param {object} doesn't have good browser support yet.
 // The first argument, locale, is a string[array], and left empty for default behavior for identifying locale
 
 module.exports = {
-	conjureChatMessage,
-	conjureRollMessage
-};
+  conjureChatMessage,
+  conjureRollMessage,
+}
